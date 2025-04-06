f301a638292736f9b9fc57f9dd0810084886e434
ketan kaushik <126436705+Alpha9903@users.noreply.github.com>
2025-04-06 14:06:18 +0530

    new folders and files structure

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the frontend folder
-app.use(express.static(path.join(__dirname, '../frontend')));
+// Serve static files from the 'public' folder inside backend
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle all routes by serving the index.html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
